c40a11497339fb843922e2d9437bafeb436b2eab
rogermasferrer <rogermasf@gmail.com>
2018-02-20 20:04:01 +0100

    Comprovar si es fa commit de services.php

--- config/services.php
+++ config/services.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-				
+
     /*
     |--------------------------------------------------------------------------
     | Third Party Services
@@ -16,7 +16,7 @@ return [
 
     'mailgun' => [
         'domain' => env('botiga.lapetitaoliba.cat'),
-        'secret' => env('key-dd97ad1cb8aabc63772898f3a4b390b3'),
+//        'secret' => env('key-dd97ad1cb8aabc63772898f3a4b390b3'),
     ],
 
     'ses' => [
